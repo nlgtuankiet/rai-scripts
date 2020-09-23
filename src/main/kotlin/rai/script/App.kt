b86ae34b423660ad03e73ad67fea2126765a8bd9
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-09-23 11:28:17 +0700

    ok

--- src/main/kotlin/rai/script/App.kt
+++ src/main/kotlin/rai/script/App.kt
@@ -5,23 +5,19 @@ package rai.script
 
 import com.google.api.gax.rpc.NotFoundException
 import com.google.auth.oauth2.ServiceAccountCredentials
-import com.google.cloud.recommendationengine.v1beta1.CatalogItem
 import com.google.cloud.recommendationengine.v1beta1.CatalogItemPathName
 import com.google.cloud.recommendationengine.v1beta1.CatalogName
 import com.google.cloud.recommendationengine.v1beta1.CatalogServiceClient
 import com.google.cloud.recommendationengine.v1beta1.CatalogServiceSettings
 import com.google.cloud.recommendationengine.v1beta1.DeleteCatalogItemRequest
 import com.google.cloud.recommendationengine.v1beta1.EventStoreName
-import com.google.cloud.recommendationengine.v1beta1.ListCatalogItemsRequest
 import com.google.cloud.recommendationengine.v1beta1.UserEventServiceClient
 import com.google.cloud.recommendationengine.v1beta1.UserEventServiceSettings
 import com.google.common.util.concurrent.RateLimiter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import java.io.File
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
@@ -47,8 +43,7 @@ fun main(args: Array<String>): Unit = runBlocking {
 
   val channel = Channel<Int>()
   val rate = (12000 / 60) - 10
-  val readRateLimiter = RateLimiter.create(rate.toDouble())
-  val inputRateLimiter = RateLimiter.create((rate + 100).toDouble())
+  val writeRateLimiter = RateLimiter.create(rate.toDouble())
 
   val firstItem = args.first().toInt()
 
@@ -70,7 +65,7 @@ fun main(args: Array<String>): Unit = runBlocking {
         val name = CatalogItemPathName
           .of("tikiandroid-1047", "global", "default_catalog", item.toString())
         try {
-          readRateLimiter.acquire()
+          writeRateLimiter.acquire()
           catalogClient.deleteCatalogItem(
             DeleteCatalogItemRequest.getDefaultInstance().toBuilder()
               .setName(name.toString())
