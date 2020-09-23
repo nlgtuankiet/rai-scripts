43fb97bd51f3547885c303eb75e17e229beebd7e
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-09-23 21:43:44 +0700

    ok

--- src/main/kotlin/rai/script/App.kt
+++ src/main/kotlin/rai/script/App.kt
@@ -75,7 +75,6 @@ fun main(args: Array<String>): Unit = runBlocking {
           .of("tikiandroid-1047", "global", "default_catalog", item.toString())
         try {
           writeRateLimiter.acquire()
-          println("after acquire $item")
           catalogClient.deleteCatalogItem(
             DeleteCatalogItemRequest.getDefaultInstance().toBuilder()
               .setName(name.toString())
